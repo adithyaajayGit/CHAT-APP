e219b23aae004101834c756648ab33dc6e51ac28
Adithya Ajay <adithyaajay098@gmail.com>
2024-09-10 23:51:11 +0530

    edit:login.jsx

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import assets from '../../assets/assets'
 
 
 const Login = () => {
+  const [currState,setCurrState] =useState("Sign up")
   return (
     <div className="login">
       <img src={assets.logo_big} className='logo' alt="Logo" />
@@ -11,7 +12,7 @@ const Login = () => {
         <source src="/115035-703067756_small.mp4" type="video/mp4" />
       </video>
       <form className='login-form'>
-        <h2>Sign up</h2>
+        <h2>{currState}</h2>
         <input type="text" placeholder='username' required className='form-input'/>
         <input type="email" placeholder='Email' required className='form-input'/>
         <input type="password" placeholder='Password' required className='form-input'/>
