e456fa1b4bb7d9f68aa046bf807044f2659e3731
Adithya Ajay <adithyaajay098@gmail.com>
2024-09-15 23:40:27 +0530

    style:leftsidebar

--- src/components/chatbox/Chatbox.jsx
+++ src/components/chatbox/Chatbox.jsx
@@ -7,6 +7,8 @@ const Chatbox = () => {
     <div className='chat-box'>
       <div className="chat-user">
         <img src={assets.profile_img} alt="" />
+        <p>Richard Sanford <img src={assets.green_dot} alt="" /></p>
+        <img src={assets.help_icon} alt="" />
       </div>
     </div>
   )
