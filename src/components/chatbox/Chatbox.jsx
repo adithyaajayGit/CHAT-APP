b1a72a136c8c95fcfb65cf4b470d341c55a10041
Adithya Ajay <adithyaajay098@gmail.com>
2024-09-15 23:41:58 +0530

    edit:chatbox.jsx

--- src/components/chatbox/Chatbox.jsx
+++ src/components/chatbox/Chatbox.jsx
@@ -8,7 +8,7 @@ const Chatbox = () => {
       <div className="chat-user">
         <img src={assets.profile_img} alt="" />
         <p>Richard Sanford <img src={assets.green_dot} alt="" /></p>
-        <img src={assets.help_icon} alt="" />
+        <img src={assets.help_icon} className='help' alt="" />
       </div>
     </div>
   )
