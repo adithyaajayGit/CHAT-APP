04c880a30874ae6fdcf99fe1dbe4bb42b00f9a0b
Adithya Ajay <adithyaajay098@gmail.com>
2024-09-15 23:39:27 +0530

    style:leftsidebar

--- src/components/chatbox/Chatbox.jsx
+++ src/components/chatbox/Chatbox.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Chatbox.css'
+import assets from '../../assets/assets'
 
 const Chatbox = () => {
   return (
     <div className='chat-box'>
       <div className="chat-user">
-        
+        <img src={assets.profile_img} alt="" />
       </div>
     </div>
   )
