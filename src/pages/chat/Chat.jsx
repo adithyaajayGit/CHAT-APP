8329a879309ac52a606e73264d03575e0902f1e5
Adithya Ajay <adithyaajay098@gmail.com>
2024-09-15 22:25:26 +0530

    add:components

--- src/pages/chat/Chat.jsx
+++ src/pages/chat/Chat.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import './Chat.css'
+import LeftSidebar from '../../components/leftsidebar/LeftSidebar'
+import RightSidebar from '../../components/rightsidebar/RightSidebar'
+import Chatbox from '../../components/chatbox/Chatbox'
 
 
 const Chat = () => {
   return (
     <div className='chat'>
-
+      <div className="chat-container">
+        <LeftSidebar />
+        <Chatbox />
+        <RightSidebar />
+      </div>
     </div>
   )
 }
