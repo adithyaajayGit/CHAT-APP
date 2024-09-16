7cc15bb10623d6ccea5eb8a26059d84698629bb7
Adithya Ajay <adithyaajay098@gmail.com>
2024-09-17 01:45:27 +0530

    add:edit-profile

--- src/lib/upload.js
+++ src/lib/upload.js
@@ -3,32 +3,38 @@ import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/
 const upload = async (file) => {
 
     const storage = getStorage();
-    const storageRef = ref(storage, 'images/rivers.jpg');
+    const storageRef = ref(storage, `images/${Date.now() + file.name}`);
 
     const uploadTask = uploadBytesResumable(storageRef, file);
 
-    uploadTask.on('state_changed',
-        (snapshot) => {
-            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            console.log('Upload is ' + progress + '% done');
-            switch (snapshot.state) {
-                case 'paused':
-                    console.log('Upload is paused');
-                    break;
-                case 'running':
-                    console.log('Upload is running');
-                    break;
-            }
-        },
-        (error) => {
+    return new Promise((resolve, reject) => {
+        uploadTask.on('state_changed',
+            (snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log('Upload is ' + progress + '% done');
+                switch (snapshot.state) {
+                    case 'paused':
+                        console.log('Upload is paused');
+                        break;
+                    case 'running':
+                        console.log('Upload is running');
+                        break;
+                }
+            },
+            (error) => {
 
-        },
-        () => {
-            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                console.log('File available at', downloadURL);
-            });
-        }
-    );
+            },
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    resolve(downloadURL)
+                });
+
+            }
+        );
+
+
+    })
 
 }
 
+export default upload;
