2127d52376ddd4d2900e6821d5a8c8e506ed2428
Adithya Ajay <adithyaajay098@gmail.com>
2024-09-18 10:13:54 +0530

    add:forgot-pswd

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
-import { doc, getFirestore, setDoc } from "firebase/firestore";
+import { createUserWithEmailAndPassword, getAuth, sendPasswordResetEmail, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { collection, doc, getDocs, getFirestore, query, setDoc, where } from "firebase/firestore";
 import { toast } from "react-toastify";
 
 const firebaseConfig = {
@@ -58,4 +58,25 @@ const logout = async () => {
     }
 }
 
-export { signup, login, logout, auth, db } 
+const resetPass = async (email) =>{
+    if (!email) {
+        toast.error("enter your email")
+        return null
+    }
+    try {
+        const userRef = collection(db,'users'); 
+        const q = query(userRef,where("email", "==", email))
+        const querySnap = await getDocs(q);
+        if (!querySnap.empty) {
+            await sendPasswordResetEmail(auth,email)
+            toast.success("Reset Email sent")
+        }else{
+            toast.error("Email doesn't exists")
+        }
+    } catch (error) {
+        console.error(error);
+        toast.error(error.message)
+    }
+}
+
+export { signup, login, logout, auth, db, resetPass } 
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,23 +1,23 @@
 import React, { useState } from 'react'
 import './Login.css'
 import assets from '../../assets/assets'
-import { signup, login } from '../../config/firebase'
+import { signup, login, resetPass } from '../../config/firebase'
 
 
 
 const Login = () => {
-  const [currState,setCurrState] =useState("Sign up")
-  const [userName,setUserName] = useState("")
-  const [email,setEmail]=useState("")
-  const [password,setPassword] = useState("")
+  const [currState, setCurrState] = useState("Sign up")
+  const [userName, setUserName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
 
-  const onSubmitHandler=(event)=>{
-      event.preventDefault();
-      if(currState=="Sign up"){
-          signup(userName,email,password)
-      }else{
-        login(email,password)
-      }
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    if (currState == "Sign up") {
+      signup(userName, email, password)
+    } else {
+      login(email, password)
+    }
   }
 
   return (
@@ -28,22 +28,27 @@ const Login = () => {
       </video>
       <form onSubmit={onSubmitHandler} className='login-form'>
         <h2>{currState}</h2>
-        { currState=="Sign up"?
-        <input type="text" onChange={(e)=>setUserName(e.target.value)} value={userName} placeholder='username' required className='form-input'/>
-        :null
+        {currState == "Sign up" ?
+          <input type="text" onChange={(e) => setUserName(e.target.value)} value={userName} placeholder='username' required className='form-input' />
+          : null
         }
-        <input type="email" onChange={(e)=>setEmail(e.target.value)} value={email} placeholder='Email' required className='form-input'/>
-        <input type="password" onChange={(e)=>setPassword(e.target.value)} value={password} placeholder='Password' required className='form-input'/>
-        <button type='submit'>{currState === "Sign up"?"Create Account":"Login now"}</button>
+        <input type="email" onChange={(e) => setEmail(e.target.value)} value={email} placeholder='Email' required className='form-input' />
+        <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} placeholder='Password' required className='form-input' />
+        <button type='submit'>{currState === "Sign up" ? "Create Account" : "Login now"}</button>
         <div className='login-term'>
-      <input type="checkbox" />
-      <p>I agree to the <span> terms of service</span></p>
+          <input type="checkbox" />
+          <p>I agree to the <span> terms of service</span></p>
         </div>
         <div className="login-forgot">
-          { currState=="Sign up"?
-          <p className='login-toggle'>Already have an account? <span className='click-here' onClick={()=>setCurrState("Login")}>Click here</span></p>
-          :
-          <p className='login-toggle'>Dont have an account? <span className='click-here' onClick={()=>setCurrState("Sign up")}>Click here</span></p>
+          {currState == "Sign up" ?
+            <p className='login-toggle'>Already have an account? <span className='click-here' onClick={() => setCurrState("Login")}>Click here</span></p>
+            :
+            <p className='login-toggle'>Dont have an account? <span className='click-here' onClick={() => setCurrState("Sign up")}>Click here</span></p>
+          }
+          {
+            currState === "Login"
+              ? <p className='login-toggle'>Forgot Password ? <span className='click-here' onClick={() => resetPass(email)}>Reset here</span></p>
+              : null
           }
         </div>
       </form>
